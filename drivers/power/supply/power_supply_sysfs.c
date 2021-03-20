66c328881aa71e43d229006f9fbb55332fb2327b
Lawliet L <zexceed12300@gmail.com>
2021-03-20 09:43:47 +0000

    drivers: power: Import xiaomi power changes
    Signed-off-by: Lawliet L <zexceed12300@gmail.com>

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -344,6 +344,8 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(cc_soc),
 	POWER_SUPPLY_ATTR(qg_vbms_mode),
 	POWER_SUPPLY_ATTR(real_capacity),
+	POWER_SUPPLY_ATTR(charging_call_state),
+	POWER_SUPPLY_ATTR(batfet_reset_enable),
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_ATTR(charge_counter_ext),
 	/* Properties of type `const char *' */
