b0705d62ff9c8db22eae7ba809cad05110768476
Quentin Perret <qperret@google.com>
2021-05-12 12:30:37 +0000

    Revert "of/fdt: Make sure no-map does not remove already reserved regions"
    This reverts commit 666ae7c255f9eb7a8fd8e55641542f3624a78b43.
    It is not really a fix, and the backport misses dependencies, which
    breaks existing platforms.
    
    Reported-by: Alexandre TORGUE <alexandre.torgue@foss.st.com>
    Signed-off-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/of/fdt.c
+++ drivers/of/fdt.c
@@ -1195,7 +1195,8 @@ int __init __weak early_init_dt_reserve_memory_arch(phys_addr_t base,
 #if defined(CONFIG_ARCH_SDM670) || defined(CONFIG_ARCH_SDM845)
 		return memblock_remove(base, size);
 #else
-		return memblock_mark_nomap(base, size);
+        if (nomap)
+		    return memblock_mark_nomap(base, size);
 #endif
 	}
 	return memblock_reserve(base, size);
