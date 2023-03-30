6ec46b212451aef5946e36d29a81393b84b3537b
r4venz <zexceed12300@gmail.com>
2023-03-30 17:07:06 +0000

    include: checkout linux/sched from 4.19

--- include/linux/sched/smt.h
+++ include/linux/sched/smt.h
@@ -2,14 +2,14 @@
 #ifndef _LINUX_SCHED_SMT_H
 #define _LINUX_SCHED_SMT_H
 
-#include <linux/atomic.h>
+#include <linux/static_key.h>
 
 #ifdef CONFIG_SCHED_SMT
-extern atomic_t sched_smt_present;
+extern struct static_key_false sched_smt_present;
 
 static __always_inline bool sched_smt_active(void)
 {
-	return atomic_read(&sched_smt_present);
+	return static_branch_likely(&sched_smt_present);
 }
 #else
 static inline bool sched_smt_active(void) { return false; }
